0ca69840ed6154af9cc3b81a5824b5bc997fda91
kartikay633 <143950873+kartikay633@users.noreply.github.com>
2026-02-07 18:41:53 +0530

    Create automated test for add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,10 @@
 
-export let cart=JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+    cart=JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
     cart=[{
@@ -13,6 +18,7 @@ if(!cart){
     quantity:1,
     deliveryOptionId:'2'
 }];
+}
 }
 
 
